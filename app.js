9e289a62165163687a8454004ae304c67e5332d7
Jarvis <jarvis@Jarvis-MacBook-Pro.local>
2021-02-13 19:25:57 +0100

    fifth_commit

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-document
-  .querySelector("#menu-btn")
-  .addEventListener("click", () =>
-    document.querySelector("body").classList.toggle("scrollDisable")
-  );
+let body = document.querySelector("body");
+let navList = document.querySelector(".site-nav-list");
+let hamTop = document.querySelector(".mobile-menu-top");
+let hamBottom = document.querySelector(".mobile-menu-bottom");
 
-document
-  .querySelector("#menu-btn")
-  .addEventListener("click", () =>
-    document.querySelector(".site-nav-list").classList.toggle("show")
-  );
+function toggleNav() {
+  body.classList.toggle("scrollDisable");
+  navList.classList.toggle("show");
+  hamTop.classList.toggle("active");
+  hamBottom.classList.toggle("active");
+}
